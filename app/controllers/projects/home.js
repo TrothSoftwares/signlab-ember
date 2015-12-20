5315ce9acb17081f24e27906d2d928e325ce1d7f
Mohammed Talha <talhaqqq@gmail.com>
2015-12-20 19:36:37 +0530

    Before adding ember-simple-auth

--- app/controllers/projects/home.js
+++ app/controllers/projects/home.js
@@ -7,7 +7,7 @@ export default Ember.Controller.extend({
   queryParams: {
     searchTerm: 's',
   },
-searchTerm: '',
+  searchTerm: '',
 
   matchingProjects: Ember.computed('model.@each.name','searchTerm', function() {
     var searchTerm = this.get('searchTerm').toLowerCase();
@@ -20,31 +20,27 @@ searchTerm: '',
 
   actions: {
     createProject: function(){
-           var controller = this;
-           var customer = this.store.peekRecord('customer',1);
-           var agent = this.store.peekRecord('agent', 1);
+      var controller = this;
+      var customer = this.store.peekRecord('customer',1);
+      var agent = this.store.peekRecord('agent', 1);
 
       var project = this.store.createRecord('project', {
-       name: this.get('name'),
-       customer : customer,
-       agent : agent});
+        name: this.get('name'),
+        customer : customer,
+        agent : agent});
 
-       var enquiry = this.store.createRecord('enquiry', {
-        date: new Date(),
-        no: '',
-        project : project});
-
-
-
-      project.save().then(function(){
-
-        enquiry.save();
-        controller.set('name' , '');
-        controller.transitionToRoute('projects.project',project);
-      });
-    }
-    // FIXME: CANNOT CREATE PROJECT
-  }
+        var enquiry = this.store.createRecord('enquiry', {
+          date: new Date(),
+          no: '',
+          project : project});
+          project.save().then(function(){
+            enquiry.save();
+            controller.set('name' , '');
+            controller.transitionToRoute('projects.project',project);
+          });
+        }
+        // FIXME: CANNOT CREATE PROJECT
+      }
 
 
-});
+    });
--- app/controllers/projects/project/enquiry-form.js
+++ app/controllers/projects/project/enquiry-form.js
@@ -6,9 +6,7 @@ export default Ember.Controller.extend({
   showCustomerComponent: false,
   showAgentComponent: false,
 
-  stepOne: true,
-  stepTwo: false,
-  stepThree: false,
+
 
   actions: {
 
@@ -18,185 +16,161 @@ export default Ember.Controller.extend({
       let itemtype = this.get('itemtypes').get('firstObject');
       let jobtype = this.get('jobtypes').get('firstObject');
       let project = this.get('project');
-      var newitem = this.store.createRecord('item', { dimensions: '', description: '', itemtype: itemtype , jobtype: jobtype, project: project });
-      newitem.save();
-      this.get('project.items').pushObject(newitem);
-    },
+      var newitem = this.store.createRecord('item', {
+        dimensions: '',
+        description: '',
+        itemtype: itemtype ,
+        jobtype: jobtype,
+        project: project });
+        newitem.save();
+        this.get('project.items').pushObject(newitem);
+      },
 
-    deleteItem: function(item){
-      item.deleteRecord();
-      item.save();
-    },
+      deleteItem: function(item){
+        item.deleteRecord();
+        item.save();
+      },
 
+      openCustomerComponent: function() {
 
+        this.toggleProperty('showCustomerComponent');
+      },
+      openAgentComponent: function() {
 
-    toStepTwo: function() {
-      this.set('stepOne', false);
-      this.set('stepTwo', true);
-    },
+        this.toggleProperty('showAgentComponent');
+      },
 
-    toStepThree: function() {
-      this.set('stepTwo', false);
-      this.set('stepThree', true);
-    },
-
-    toStepOne: function(){
-      this.set('stepThree', false);
-      this.set('stepOne', true);
-    },
-    openCustomerComponent: function() {
-
-      this.toggleProperty('showCustomerComponent');
-    },
-    openAgentComponent: function() {
-
-      this.toggleProperty('showAgentComponent');
-    },
-
-
-    onToggle:function(){
-      if(this.model.get('hasDirtyAttributes')){
+      deleteData:function(){
+        var controller = this;
+        this.model.deleteRecord();
         this.model.save();
+        controller.transitionToRoute('customers.new');
+      },
+
+      onSelectCustomer(customer) {
+        this.set('project.customer', customer);
+      },
+
+      onSelectAgent(agent) {
+        this.set('project.agent', agent);
+      },
+
+      onSelectItemtypes(index , itemType){
+        this.get('project.items').objectAt(index).set('itemtype', itemType);
+      },
+
+      onSelectJobtypes(index , jobType  ){
+        this.get('project.items').objectAt(index).set('jobtype',jobType);
+      },
+
+
+      createCustomer: function() {
+        var controller = this;
+        var customer = this.store.createRecord('customer', {
+          name :this.get('custname'),
+          contactname :this.get('custcontactname'),
+          contactno :this.get('custcontactno'),
+          othcontactno :this.get('custothcontactno'),
+          othrefdetails:this.get('custothrefdetails'),
+        });
+        customer.save().then(function(){
+          controller.set('custname','');
+          controller.set('custcontactname','');
+          controller.set('custcontactno','');
+          controller.set('custothcontactno','');
+          controller.set('custothrefdetails','');
+          controller.send('onSelectCustomer',customer);
+          controller.set('showCustomerComponent' ,false);
+          controller.notifications.addNotification({
+            message: 'Customer Created successfully!' ,
+            type: 'success',
+            autoClear: true
+          });
+        });
+      },
+
+
+      createAgent: function() {
+        var controller = this;
+        var agent = this.store.createRecord('agent', {
+          name :this.get('agentname'),
+          contactname :this.get('agentcontactname'),
+          contactno :this.get('agentcontactno'),
+          othcontactno :this.get('agentothcontactno'),
+          othrefdetails:this.get('agentothrefdetails'),
+        });
+
+        agent.save().then(function(){
+          controller.set('agentname','');
+          controller.set('agentcontactname','');
+          controller.set('agentcontactno','');
+          controller.set('agentothcontactno','');
+          controller.set('agentothrefdetails','');
+          controller.send('onSelectAgent',agent);
+          controller.set('showAgentComponent' ,false);
+          controller.notifications.addNotification({
+            message: 'agent Created successfully!' ,
+            type: 'success',
+            autoClear: true
+          });
+        });
+      },
+
+
+      saveProject:function(){
+
+
+
+        var controller= this;
+        var  project = controller.get('project');
+        var customer = controller.get('project.customer');
+        var agent = controller.get('project.agent');
+        var currentEnquiry = project.get('enquiries');
+        currentEnquiry.forEach(function(curenq){
+          curenq.save();
+        });
+        //var newproject = this.store.createRecord('project' , { name: 'Sasi project', customer: customer , agent:agent});
+        //project.set('enquiry',)
+
+        project.set('customer', customer );
+        project.set('agent', agent );
+
+        var currentItems = project.get('items');
+        currentItems.forEach(function(curitem){
+          curitem.save();
+        });
+
+
+        var itemtype = this.store.peekRecord('itemtype', 1);
+
+        // var jobtypes = this.store.peekAll('jobtype');
+        // console.log(JSON.stringify(jobtypesq));
+        // console.log(jobtypesq);
+        // var newitem = this.store.createRecord('item' ,
+        // { dimensions: 'test dimensions', description: 'test description' , project:  project , itemtype: itemtype , jobtypes: jobtypes  });
+        // console.log('++++++' + newitem.get('jobtypes').resolve());
+        // newitem.get('jobtypes').then(function(jobtys){
+        //   jobtys.pushObject(jobtypes);
+        // });
+        //  newitem.get('jobtypes').pushObject(jobtypes);
+        // newitem.save();
+        // let item = this.store.peekRecord('item', 1);
+        // let jobtype = this.store.peekRecord('jobtype', 1);
+        // item.get('jobtypes').pushObject(jobtype);
+        // item.save();
+
+        return project.save().then(function(){
+          controller.notifications.addNotification({
+            message: 'Project Saved!' ,
+            type: 'success',
+            autoClear: true
+          });
+
+        });
+
+
       }
-    },
-    deleteData:function(){
-      var controller = this;
-      this.model.deleteRecord();
-      this.model.save();
-      controller.transitionToRoute('customers.new');
-    },
-    onSelectCustomer(customer) {
-      this.set('project.customer', customer);
 
     },
 
-    onSelectAgent(agent) {
-      this.set('project.agent', agent);
-    },
-
-    onSelectItemtypes(index , itemType){
-      this.get('project.items').objectAt(index).set('itemtype', itemType);
-    },
-
-    onSelectJobtypes(index , jobType  ){
-      this.get('project.items').objectAt(index).set('jobtype',jobType);
-    },
-
-
-    createCustomer: function() {
-      var controller = this;
-      var customer = this.store.createRecord('customer', {
-        name :this.get('custname'),
-        contactname :this.get('custcontactname'),
-        contactno :this.get('custcontactno'),
-        othcontactno :this.get('custothcontactno'),
-        othrefdetails:this.get('custothrefdetails'),
-      });
-      customer.save().then(function(){
-
-        controller.set('custname','');
-        controller.set('custcontactname','');
-        controller.set('custcontactno','');
-        controller.set('custothcontactno','');
-        controller.set('custothrefdetails','');
-
-
-        controller.send('onSelectCustomer',customer);
-        controller.set('showCustomerComponent' ,false);
-
-        controller.notifications.addNotification({
-          message: 'Customer Created successfully!' ,
-          type: 'success',
-          autoClear: true
-        });
-      });
-    },
-
-
-    createAgent: function() {
-      var controller = this;
-      var agent = this.store.createRecord('agent', {
-        name :this.get('agentname'),
-        contactname :this.get('agentcontactname'),
-        contactno :this.get('agentcontactno'),
-        othcontactno :this.get('agentothcontactno'),
-        othrefdetails:this.get('agentothrefdetails'),
-      });
-      agent.save().then(function(){
-
-        controller.set('agentname','');
-        controller.set('agentcontactname','');
-        controller.set('agentcontactno','');
-        controller.set('agentothcontactno','');
-        controller.set('agentothrefdetails','');
-
-
-        controller.send('onSelectAgent',agent);
-        controller.set('showAgentComponent' ,false);
-
-        controller.notifications.addNotification({
-          message: 'agent Created successfully!' ,
-          type: 'success',
-          autoClear: true
-        });
-      });
-    },
-
-
-    saveProject:function(){
-
-
-
-      var controller= this;
-      var  project = controller.get('project');
-      var customer = controller.get('project.customer');
-      var agent = controller.get('project.agent');
-      var currentEnquiry = project.get('enquiries');
-      currentEnquiry.forEach(function(curenq){
-        curenq.save();
-      });
-      //var newproject = this.store.createRecord('project' , { name: 'Sasi project', customer: customer , agent:agent});
-      //project.set('enquiry',)
-
-      project.set('customer', customer );
-      project.set('agent', agent );
-
-      var currentItems = project.get('items');
-      currentItems.forEach(function(curitem){
-        curitem.save();
-      });
-
-
-      var itemtype = this.store.peekRecord('itemtype', 1);
-
-      // var jobtypes = this.store.peekAll('jobtype');
-      // console.log(JSON.stringify(jobtypesq));
-      // console.log(jobtypesq);
-      // var newitem = this.store.createRecord('item' ,
-      // { dimensions: 'test dimensions', description: 'test description' , project:  project , itemtype: itemtype , jobtypes: jobtypes  });
-      // console.log('++++++' + newitem.get('jobtypes').resolve());
-      // newitem.get('jobtypes').then(function(jobtys){
-      //   jobtys.pushObject(jobtypes);
-      // });
-      //  newitem.get('jobtypes').pushObject(jobtypes);
-      // newitem.save();
-      // let item = this.store.peekRecord('item', 1);
-      // let jobtype = this.store.peekRecord('jobtype', 1);
-      // item.get('jobtypes').pushObject(jobtype);
-      // item.save();
-
-      return project.save().then(function(){
-        controller.notifications.addNotification({
-          message: 'Project Saved!' ,
-          type: 'success',
-          autoClear: true
-        });
-
-      });
-
-
-    }
-
-  },
-
-});
+  });
