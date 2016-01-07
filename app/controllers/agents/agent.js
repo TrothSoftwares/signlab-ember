85d10d12e902b8b4e86af8775395d734e76b27a0
Mohammed Talha <talhaqqq@gmail.com>
2016-01-07 22:18:49 +0530

    editAgent function fixes

--- app/controllers/agents/agent.js
+++ app/controllers/agents/agent.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+
   actions: {
-    editagent :function(){
-  // FIXME : Data is saving even when the name field is empty.. (Should be fixed at the agent-form too.)
+    editAgent :function(){
       this.toggleProperty('enableEditagent');
       if(this.model.get('hasDirtyAttributes')){
         this.model.save();
