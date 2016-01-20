f8e7819c4c5497c2e7af07ab65d91f05125fe41a
Mohammed Talha <talhaqqq@gmail.com>
2016-01-20 08:30:59 +0530

    After route authorization

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
-  session: Ember.inject.service('session'),
+
+session: Ember.inject.service('session'),
 
   actions: {
     logout() {
--- app/routes/dashboard/projects/project/enquiry-form.js
+++ app/routes/dashboard/projects/project/enquiry-form.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
+  
+
   model: function() {
     return Ember.RSVP.hash({  // RSVP.hash for resolving multiple models, The setupController will only work until all promises resolved.
       project: this.modelFor('dashboard.projects.project'),
